67d554ba3e4fb1eb73c24552ee97131c3995b68d
Tom Bogin <tombo@monday.com>
2021-12-25 19:58:58 +0200

    remove resolver exports

--- src/spy-webpack/webpack-spier.ts
+++ src/spy-webpack/webpack-spier.ts
@@ -1,11 +1,11 @@
 /* tslint:disable:no-console */
-export function spyWebpackAliases(aliases: {[key: string]: string} = {}) {
+export function spyWebpackAliases(nodemodule: string, aliases: string[]) {
     const finalAliases: {[key: string]: string} = {};
     const supported = new Set(["redux-thunk", "reselect", "re-reselect", "redux"])
-    for(const [aliasKey, aliasNodeModulePath] of Object.entries(aliases)) {
+    for(const aliasKey of aliases) {
         if(supported.has(aliasKey)) {
-            finalAliases[aliasKey+"-original"] = aliasNodeModulePath;
-            finalAliases[aliasKey] = "performance-spy/resolver/"+aliasKey+"-module";
+            finalAliases[aliasKey+"-original"] = nodemodule+"/"+aliasKey;
+            finalAliases[aliasKey] = nodemodule+"/performance-spy/resolver/"+aliasKey+"-module";
         }
     }
     console.log("Overriden libraries by webpack performance spier", finalAliases);
