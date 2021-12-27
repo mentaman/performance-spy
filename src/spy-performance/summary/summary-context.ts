05a511900430717f6296ece3f7b56dcaa683ab76
Tom <bogin7@gmail.com>
2021-12-27 12:40:38 +0200

    context with domain

--- src/spy-performance/summary/summary-context.ts
+++ src/spy-performance/summary/summary-context.ts
@@ -1,7 +1,18 @@
+import domain from "domain";
 import { Summaries } from "./summaries";
 
 export const GlobalSummaries = new Summaries();
 
+export const getSummaryFor = (func: () => void) => {
+    const summaries = new Summaries();
+    const d = domain.create();
+    (d as any).summaries = summaries;
+    d.run(() => {
+        func();
+    })
+    return summaries.getSummary();
+}
+
 export const getCurrentSummaryForUse = (): Summaries => {
-    return GlobalSummaries;
+    return (process as any)?.domain?.summaries || GlobalSummaries;
 };
