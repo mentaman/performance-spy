6a586b4af53c1e9ba51e523257cf87de3279d464
Tom Bogin <tombo@monday.com>
2022-12-19 07:20:17 +0200

    cached selector resultFunc

--- examples/jest-example/re-reselect-jest.js
+++ examples/jest-example/re-reselect-jest.js
@@ -6,56 +6,72 @@ const keyOne = "a";
 const keyTwo = "b";
 
 describe("re-reselect performance spying", () => {
-    beforeEach(() => {
-        selectorExample = createCachedSelector((a, _b, _key) => a, (_a, b, _key) => b, (_resA, _resB) => 100)((_a, _b, key) => key);
-    })
+  beforeEach(() => {
+    selectorExample = createCachedSelector(
+      (a, _b, _key) => a,
+      (_a, b, _key) => b,
+      (_resA, _resB) => 100
+    )((_a, _b, key) => key);
+  });
 
-    it("should find only one combiner calculation", () => {
-        selectorExample(1, 2, keyOne);
-        selectorExample(1, 2, keyTwo);
+  it("should find only one combiner calculation", () => {
+    selectorExample(1, 2, keyOne);
+    selectorExample(1, 2, keyTwo);
 
-        const summary = getPerfSummary();
+    const summary = getPerfSummary();
 
-        expect(summary.mostCalculatedCombiners.length).toEqual(1);
-    })
+    expect(summary.mostCalculatedCombiners.length).toEqual(1);
+  });
 
-    it("should calculate only once", () => {
-        selectorExample(1, 2, keyOne);
-        selectorExample(1, 2, keyOne);
-        selectorExample(2, 3, keyTwo);
-        selectorExample(2, 3, keyTwo);
+  it("should calculate only once", () => {
+    selectorExample(1, 2, keyOne);
+    selectorExample(1, 2, keyOne);
+    selectorExample(2, 3, keyTwo);
+    selectorExample(2, 3, keyTwo);
 
-        const summary = getPerfSummary();
+    const summary = getPerfSummary();
 
-        expect(summary.mostCalculatedCombiners[0].maxCount).toEqual(1);
-    })
+    expect(summary.mostCalculatedCombiners[0].maxCount).toEqual(1);
+  });
 
-    it("should calculate three times in each one", () => {
-        selectorExample(1, 2, keyOne);
-        selectorExample(1, 3, keyOne);
-        selectorExample(1, 2, keyOne);
+  it("should calculate three times in each one", () => {
+    selectorExample(1, 2, keyOne);
+    selectorExample(1, 3, keyOne);
+    selectorExample(1, 2, keyOne);
 
-        selectorExample(1, 2, keyTwo);
-        selectorExample(1, 3, keyTwo);
-        selectorExample(1, 2, keyTwo);
+    selectorExample(1, 2, keyTwo);
+    selectorExample(1, 3, keyTwo);
+    selectorExample(1, 2, keyTwo);
 
-        const summary = getPerfSummary();
+    const summary = getPerfSummary();
 
-        expect(summary.mostCalculatedCombiners[0].maxCount).toEqual(3);
-        expect(summary.mostCalculatedCombiners[0].count).toEqual(6);
-    })
+    expect(summary.mostCalculatedCombiners[0].maxCount).toEqual(3);
+    expect(summary.mostCalculatedCombiners[0].count).toEqual(6);
+  });
 
-    it("should find which param caused it to recalculate", () => {
-        selectorExample(1, 2, keyOne);
-        selectorExample(1, 3, keyOne);
-        selectorExample(1, 2, keyOne);
+  it("should find which param caused it to recalculate", () => {
+    selectorExample(1, 2, keyOne);
+    selectorExample(1, 3, keyOne);
+    selectorExample(1, 2, keyOne);
 
-        selectorExample(1, 2, keyTwo);
-        selectorExample(1, 3, keyTwo);
-        selectorExample(1, 2, keyTwo);
+    selectorExample(1, 2, keyTwo);
+    selectorExample(1, 3, keyTwo);
+    selectorExample(1, 2, keyTwo);
 
-        const summary = getPerfSummary();
+    const summary = getPerfSummary();
 
-        expect(summary.mostCalculatedCombiners[0].args.stats['1'].count).toEqual(4);
-    })
-})
+    expect(summary.mostCalculatedCombiners[0].args.stats["1"].count).toEqual(4);
+  });
+
+  it("let override selector for tests", () => {
+    const summarySelector = createCachedSelector(
+      (a, _b, _key) => 0,
+      (_a, b, _key) => 0,
+      (resA, resB) => resA + resB
+    )((_a, _b, key) => key);
+    const combiner = summarySelector.resultFunc;
+
+    const res = combiner(10, 10);
+    expect(res).toEqual(20);
+  });
+});
